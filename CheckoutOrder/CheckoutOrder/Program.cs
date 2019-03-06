05cc1d89320f117b95d46b3c9301cd9e5333bde6
ApplicationDeveloper <eagle-github@usa.net>
2019-03-06 13:46:14 -0600

    Initial creation of the CheckoutOrder solution.

---
+++ CheckoutOrder/CheckoutOrder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutOrder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program programToRun = new Program();
+            programToRun.ScanItem();
+        }
+
+        public double ScanItem()
+        {
+            return 0.0;
+        }
+    }
+
+
+}
---
+++ CheckoutOrder/CheckoutOrderTest/CheckoutOrderTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CheckoutOrder;
+
+namespace CheckoutOrderTest
+{
+    public class CheckoutOrderTests
+    {
+        private Program _checkoutOrderUnderTest = null;
+        public CheckoutOrderTests()
+        {
+            _checkoutOrderUnderTest = new Program();
+        }
+
+        [Fact]
+        public void ScanItem()
+        {
+            double totalBill = _checkoutOrderUnderTest.ScanItem();
+            Assert.Equal(0, totalBill);
+        }
+    }
+}
