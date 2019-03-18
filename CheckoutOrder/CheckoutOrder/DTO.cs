743181fdc2f8a6c43c848a20820b68e584b291a8
ApplicationDeveloper <eagle-github@usa.net>
2019-03-18 09:27:08 -0500

    Refactor: Moved DTO classes to their own code file.  They are small, so I put them all in one cs file for simplicity.

---
+++ CheckoutOrder/CheckoutOrder/DTO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutOrder
+{
+    public class StockItem
+    {
+        public string ItemName { get; set; }
+        public double UnitPrice { get; set; }
+        public double Markdown { get; set; }
+        public string PriceCategory { get; set; }
+        public QuantityDiscount QtyDiscount { get; set; }
+        public GroupDiscount GrpDiscount { get; set; }
+    }
+
+    public class QuantityDiscount
+    {
+        public int FullPriceItems { get; set; }
+        public int QuantityUnderDiscount { get; set; }
+        public double Discount { get; set; }
+        public int MaximumNumberOfDiscounts { get; set; }
+
+        public QuantityDiscount()
+        {
+            MaximumNumberOfDiscounts = -1;
+        }
+    }
+
+    public class GroupDiscount
+    {
+        public int QuantityToGetDiscount { get; set; }
+        public double PriceForGroup { get; set; }
+        public int MaxNumberOfDiscounts { get; set; }
+    }
+
+    public class ShoppingCartItem : IComparable<ShoppingCartItem>
+    {
+        public double UnitPrice { get; set; }
+        public double ItemPrice { get; set; }
+        public double ItemWeight { get; set; }
+
+        // Sort in descending order
+        public int CompareTo(ShoppingCartItem itemToCompare)
+        {
+            return itemToCompare.ItemPrice.CompareTo(this.ItemPrice);
+        }
+    }
+}
--- CheckoutOrder/CheckoutOrder/Program.cs
+++ CheckoutOrder/CheckoutOrder/Program.cs
@@ -139,36 +139,6 @@ namespace CheckoutOrder
         }
     }
 
-    public class StockItem
-    {
-        public string ItemName { get; set; }
-        public double UnitPrice { get; set; }
-        public double Markdown { get; set; }
-        public string PriceCategory { get; set; }
-        public QuantityDiscount QtyDiscount { get; set; }
-        public GroupDiscount GrpDiscount { get; set; }
-    }
-
-    public class QuantityDiscount
-    {
-        public int FullPriceItems { get; set; }
-        public int QuantityUnderDiscount { get; set; }
-        public double Discount { get; set; }
-        public int MaximumNumberOfDiscounts { get; set; }
-
-        public QuantityDiscount()
-        {
-            MaximumNumberOfDiscounts = -1;
-        }
-    }
-
-    public class GroupDiscount
-    {
-        public int QuantityToGetDiscount { get; set; }
-        public double PriceForGroup { get; set; }
-        public int MaxNumberOfDiscounts { get; set; }
-    }
-
     public class ShoppingCartItemHolder
     {
         public IList<ShoppingCartItem> CartItems { get; set; }
@@ -211,19 +181,6 @@ namespace CheckoutOrder
                 ItemWeight = itemWeight
             };
             this.CartItems.Add(currentItemBeingScanned);
-        }
-    }
-
-    public class ShoppingCartItem : IComparable<ShoppingCartItem>
-    {
-        public double UnitPrice { get; set; }
-        public double ItemPrice { get; set; }
-        public double ItemWeight { get; set; }
-
-        // Sort in descending order
-        public int CompareTo(ShoppingCartItem itemToCompare)
-        {
-            return itemToCompare.ItemPrice.CompareTo(this.ItemPrice);
         }
     }
 }
